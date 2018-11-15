1da0752713b14415fcb7c1abc231a081784f092b
Cheung <zhangyongjian.love@qq.com>
2018-11-15 13:48:20 +0800

    完成详情页banner

--- src/common/gallery/Gallery.vue
+++ src/common/gallery/Gallery.vue
@@ -1,13 +1,10 @@
 <template>
-    <div class="container">
+    <div class="container" @click="handleGalleryClick">
         <div class="wrapper">
             <swiper :options="swiperOptions">
                 <!-- slides -->
-                <swiper-slide>
-                    <img class="gallary-img" src="//img1.qunarzz.com/sight/p0/1810/2b/2bb1fc603cf64c50a3.img.jpg_r_800x800_66b109ff.jpg" alt="slide">
-                </swiper-slide>
-                <swiper-slide>
-                    <img class="gallary-img" src="//img1.qunarzz.com/sight/p0/1810/e5/e5418560e6b5c7c8a3.img.jpg_r_800x800_7593ce0a.jpg" alt="slide">
+                <swiper-slide v-for="(img, index) in imgs" :key="index">
+                    <img class="gallary-img" :src="img" alt="slide">
                 </swiper-slide>
                 <div class="swiper-pagination"  slot="pagination"></div>
             </swiper>
@@ -17,13 +14,28 @@
 
 <script>
 export default {
-  name: 'CommonGallary',
+  name: 'CommonGallery',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
   data () {
     return {
       swiperOptions: {
         pagination: '.swiper-pagination',
-        paginationType: 'fraction'
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
       }
+    }
+  },
+  methods: {
+    handleGalleryClick () {
+      this.$emit('galleryClick')
     }
   }
 }
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,23 +1,37 @@
 <template>
     <div>
-        <div class="banner">
+        <div class="banner" @click="handleBannerClick">
             <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1810/2b/2bb1fc603cf64c50a3.img.jpg_600x330_9a7b8cf2.jpg" alt="喜运来温泉">
             <div class="banner-info">
                 <div class="banner-number"><span class="iconfont banner-icon">&#xe602;</span>28</div>
                 <div class="banner-title">喜运来温泉</div>
             </div>
         </div>
-        <common-gallary></common-gallary>
+        <common-gallery :imgs="imgs" v-show="showGallery" @galleryClick="handleGalleryClick"></common-gallery>
     </div>
 
 </template>
 
 <script>
-import CommonGallary from 'common/gallary/Gallary'
+import CommonGallery from 'common/gallery/Gallery'
 export default {
   name: 'DetailBanner',
+  data () {
+    return {
+      imgs: ['//img1.qunarzz.com/sight/p0/1810/2b/2bb1fc603cf64c50a3.img.jpg_r_800x800_66b109ff.jpg', '//img1.qunarzz.com/sight/p0/1810/e5/e5418560e6b5c7c8a3.img.jpg_r_800x800_7593ce0a.jpg'],
+      showGallery: false
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallery = true
+    },
+    handleGalleryClick () {
+      this.showGallery = false
+    }
+  },
   components: {
-    CommonGallary
+    CommonGallery
   }
 }
 </script>
