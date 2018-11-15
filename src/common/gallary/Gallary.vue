762784ea113f4af62bbcae8b0d460e686f7cc52f
Cheung <zhangyongjian.love@qq.com>
2018-11-15 08:02:18 +0800

    上课  写到gallary 公用组件

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+        <div class="wrapper">
+            <swiper :options="swiperOptions">
+                <!-- slides -->
+                <swiper-slide>
+                    <img class="gallary-img" src="//img1.qunarzz.com/sight/p0/1810/2b/2bb1fc603cf64c50a3.img.jpg_r_800x800_66b109ff.jpg" alt="slide">
+                </swiper-slide>
+                <swiper-slide>
+                    <img class="gallary-img" src="//img1.qunarzz.com/sight/p0/1810/e5/e5418560e6b5c7c8a3.img.jpg_r_800x800_7593ce0a.jpg" alt="slide">
+                </swiper-slide>
+                <div class="swiper-pagination"  slot="pagination"></div>
+            </swiper>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+    .container >>> .swiper-container
+        overflow inherit
+    .container
+        display flex
+        flex-direction column
+        justify-content center
+        position fixed
+        left 0
+        right 0
+        top 0
+        bottom 0
+        background #000
+        z-index 99
+        .wrapper
+            width 100%
+            height 0
+            padding-bottom 100%
+            .gallary-img
+                width 100%
+            .swiper-pagination
+                color #fff
+                bottom -1rem
+
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="banner">
+            <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1810/2b/2bb1fc603cf64c50a3.img.jpg_600x330_9a7b8cf2.jpg" alt="喜运来温泉">
+            <div class="banner-info">
+                <div class="banner-number"><span class="iconfont banner-icon">&#xe602;</span>28</div>
+                <div class="banner-title">喜运来温泉</div>
+            </div>
+        </div>
+        <common-gallary></common-gallary>
+    </div>
+
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+    .banner
+        position relative
+        overflow hidden
+        height 0
+        padding-bottom 55%
+        .banner-img
+            width 100%
+        .banner-info
+            display flex
+            position absolute
+            left .1rem
+            right 0
+            bottom 0
+            padding .16rem 0
+            line-height .6rem
+            color #fff
+            background-image linear-gradient(top, rgba(0,0,0,0),rgba(0,0,0,0.4))
+            .banner-title
+                flex 1
+                font-size .36rem
+                padding 0 .2rem
+            .banner-number
+                position absolute
+                bottom .74rem
+                left .2rem
+                padding .02rem .3rem
+                font-size .24rem
+                line-height .4rem
+                height .4rem
+                border-radius .2rem
+                background rgba(0,0,0,.5)
+                .banner-icon
+                    font-size .24rem
+                    padding 0 .08rem
+</style>
